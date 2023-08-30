82c931caae44c2de111a1d78996fe1eb7d0b5c74
Rajarshi38 <rajarshibiswas38@gmail.com>
2023-08-30 13:13:23 +0530

    removed unnecessary config for vite

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ["leaflet", "leaflet/dist", "leaflet/dist/images"],
-  },
   server: {
     port: 3000,
     host: true,
